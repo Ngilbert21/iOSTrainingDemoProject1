880f584ba971087565a602a2fb283009b2b174d2
Nick <Nicholasgilbert21@yahoo.com>
2021-02-03 21:10:48 -0500

    Initial Commit

---
+++ iOSTrainingDemoProject1/TrainingClass.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingClass.swift
+//  iOSTrainingDemoProject1
+//
+//  Created by Nicholas Gilbert on 2/3/21.
+//  Copyright © 2021 Nicholas Gilbert. All rights reserved.
+//
+
+import Foundation
+
+class TrainingClass {
+    
+}
